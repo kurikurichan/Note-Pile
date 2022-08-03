e2310ef2e9589bfe0c17981cb7f24ffad78f6a9d
Krista Strucke <kstrucke@gmail.com>
2022-08-02 17:04:02 -0700

    fixed dockerfile

---
+++ react-app/src/store/notebooks.js
@@ -0,0 +1,6 @@
+const GET_ALL_NOTEBOOKS = "notebooks/GET_ALL_NOTEBOOKS";
+const CREATE_NOTEBOOK = "notebooks/CREATE_NOTEBOOK";
+const UPDATE_NOTEBOOK = "notebooks/UPDATE_NOTEBOOK";
+const DELETE_NOTEBOOK = "notebooks/DELETE_NOTEBOOK";
+
+const getYourNotebooks
