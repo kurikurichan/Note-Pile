9a3b31d747c44fc0fc58728a48f87d4fd04bf8bc
Krista Strucke <kstrucke@gmail.com>
2022-08-06 12:36:03 -0700

    set up home divs

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -4,7 +4,20 @@ import './Home.css';
 export default function Home() {
   return (
     <div className="home-container">
-        <h1>Home</h1>
+        <div className="home-title">
+            <h1>Home</h1>
+        </div>
+        <div className="underlying-photo">
+
+        </div>
+        <div className="contain-widgets">
+            <div className="notes-preview">
+
+            </div>
+            <div className="scratch">
+
+            </div>
+        </div>
     </div>
   )
 }
