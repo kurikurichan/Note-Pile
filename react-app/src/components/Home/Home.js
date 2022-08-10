f30a192bef852e3ed82d9f194e48c260acdbd177
Krista Strucke <kstrucke@gmail.com>
2022-08-10 11:54:15 -0700

    home css

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -1,22 +1,67 @@
 import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+
 import './Home.css';
 
+import coffee from './coffee.jpeg';
+
 export default function Home() {
+
+  const user = useSelector(state => state.session.user);
+
+  const dispatch = useDispatch();
+
+    //  get and format long date
+    const getToday = () => {
+      const today = new Date();
+      const weekdays = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ]
+      return weekdays[today.getDay()] + ", " +
+      today.toLocaleDateString('en-CA', {
+          dateStyle: "long"
+      });
+  };
+
   return (
     <div className="home-container">
-        <div className="home-title">
-            <h1>Home</h1>
-        </div>
-        <div className="underlying-photo">
+      <div className="scrollable">
+          <div className="above-home">
 
-        </div>
-        <div className="contain-widgets">
-            <div className="notes-preview">
+            <div className="underlying-photo">
+                <img src={coffee} id="coffee" alt="blurred cup in background" />
+              </div>
 
+            <div className="above-home-text">
+              <h3>Welcome, {user.username}!</h3>
+              <p id="today">{getToday()}</p>
             </div>
-            <div className="scratch">
 
-            </div>
+
+
+          </div>
+
+          <div style={{ padding: '15%'}}></div>
+          <div className="contain-widgets">
+
+              <div className="notes-preview">
+                <div className="notes-preview-top">
+                  <p>NOTES<i className="fa-solid fa-angle-right"></i></p>
+                </div>
+                <div className="notes-preview-display">
+
+                </div>
+              </div>
+
+              <div className="scratch">
+              </div>
+          </div>
         </div>
     </div>
   )
