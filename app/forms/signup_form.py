6da7003d72df76378f95c69c31f48864575973a5
Krista Strucke <kstrucke@gmail.com>
2022-09-12 00:53:34 -0700

    added pw validation

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -19,9 +19,15 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+def password_len(form, field):
+    # Checking if password length is at least 4
+    password = field.data
+    if len(password) < 5:
+        raise ValidationError('Password should be at least 5 characters long.')
+
 
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired("Username is required"), username_exists])
     email = StringField('email', validators=[DataRequired("Email address is required"), user_exists, Email("Invalid email address")])
-    password = StringField('password', validators=[DataRequired("Password is required")])
+    password = StringField('password', validators=[DataRequired("Password is required"), password_len])
