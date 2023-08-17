39e626df2e24d6d43c80ab3fb57884949c0947a5
Krista Strucke <kstrucke@gmail.com>
2023-08-17 01:10:37 -0700

    animation on notebook dropdown

--- react-app/src/components/Sidebar/DropdownMenu.js
+++ react-app/src/components/Sidebar/DropdownMenu.js
@@ -1,5 +1,6 @@
 import CreateNBModal from "../Home/CreateNBModal";
 import { NavLink } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion/dist/framer-motion";
 
 export default function DropdownMenu({ noteDropdown, notebooks, user }) {
   const formatTitle = (name) => {
@@ -11,20 +12,32 @@ export default function DropdownMenu({ noteDropdown, notebooks, user }) {
   };
 
   return (
-    <div className={`notebook-dropdown${noteDropdown ? "-open" : ""}`}>
-      {Object.values(notebooks).map((book) => (
-        <NavLink
-          to={`/${book.id}/recent`}
-          key={book.id}
-          className="notebook-li"
-          activeClassName="sb-active"
-        >
-          {formatTitle(book.title)}
-        </NavLink>
-      ))}
-      {/* {!Object.values(notebooks).length && <p className="notebook-li">Shelf is empty</p>} */}
+    <>
+      {noteDropdown && (
+        <AnimatePresence>
+          <motion.div
+            className="notebook-dropdown"
+            initial={{ opacity: 0, y: 0 }}
+            animate={{ opacity: 1, y: 10 }}
+            exit={{ opacity: 0, y: -10, transition: { delay: 0.05 } }}
+            transition={{ type: "spring", duration: 0.2, ease: "easeInOut" }}
+          >
+            {Object.values(notebooks).map((book) => (
+              <NavLink
+                to={`/${book.id}/recent`}
+                key={book.id}
+                className="notebook-li"
+                activeClassName="sb-active"
+              >
+                {formatTitle(book.title)}
+              </NavLink>
+            ))}
+            {/* {!Object.values(notebooks).length && <p className="notebook-li">Shelf is empty</p>} */}
 
-      <CreateNBModal user={user} />
-    </div>
+            <CreateNBModal user={user} />
+          </motion.div>
+        </AnimatePresence>
+      )}
+    </>
   );
 }
